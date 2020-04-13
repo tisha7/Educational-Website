06a50b9257a31bfe6b85a3c36a809e4ed435e6e7
Ishrat Jahan Tisha <47575896+tisha7@users.noreply.github.com>
2020-04-14 04:02:58 +0600

    Add files via upload

---
+++ unistudy/admin/includes/dbcon.php
@@ -0,0 +1,8 @@
+<?php
+$server_host='localhost';
+$user='root';
+$pass='';
+$dbname='edu';
+$con=mysqli_connect($server_host,$user,$pass,$dbname);
+?>
+    
---
+++ unistudy/admin/includes/sidebar.php
@@ -0,0 +1,21 @@
+<div class="container-fluid-full">
+		<div class="row-fluid">
+			<div id="sidebar-left" class="span2">
+				<div class="nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li><a href="index.php"><i class="icon-bar-chart"></i><span class="hidden-tablet">Dashboard</span></a></li>	
+						<li><a href="user.php"><i class="icon-user"></i><span class="hidden-tablet">Register Admin</span></a></li>
+						<li><a href="userchangefinal.php"><i class="icon-info-sign"></i><span class="hidden-tablet">User Information</span></a></li>
+						<li><a class="dropmenu" href="#"><i class="icon-reorder"></i><span class="hidden-tablet">Blog</span></a>
+							<ul>
+								<li><a class="submenu" href="makepost.php"><i class="icon-file-alt"></i><span class="hidden-tablet">Create</span></a></li>
+								<li><a class="submenu" href="#"><i class="icon-file-alt"></i><span class="hidden-tablet"> Sub Menu 2</span></a></li>
+								<li><a class="submenu" href="#"><i class="icon-file-alt"></i><span class="hidden-tablet"> Sub Menu 3</span></a></li>
+							</ul>	
+						</li>
+						<li><a href="visitorchangefinal.php"><i class="icon-info-sign"></i><span class="hidden-tablet">Visitor Messages</span></a></li>
+						<li><a href="http://localhost/UniStudy/"><i class="icon-globe"></i><span class="hidden-tablet">Visit Website</span></a></li>
+						<li><a href="index.php"><i class="icon-off"></i><span class="hidden-tablet">Logout</span></a></li>
+					</ul>
+				</div>
+			</div>
